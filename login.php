8bc73b3032a7efe1774fb2d8834f6ac11f0fbfdd
Sharma1073 <105736944+Sharma1073@users.noreply.github.com>
2022-05-25 22:05:07 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
+
 
 <?php
-ECHO "Hello World!<br>";
-echo "Hello World!<br>";
-EcHo "Hello World!<br>";
+ECHO "WELCOME";
+// echo "Hello World!<br>";
+// EcHo "Hello World!<br>";
 ?>
-
-</body>
-</html>
